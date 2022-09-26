f7852457d0176d8cbb8e25639dc888fa68033596
Joeunji0119 <95282989+Joeunji0119@users.noreply.github.com>
2022-09-26 14:07:42 +0900

    feat : login fin

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as S from './LoginStyle';
+import { API } from '../../../config';
+
+const Login = () => {
+  const [info, setInfo] = useState({
+    userId: '',
+    userPassword: '',
+  });
+
+  const onChangeinfo = e => {
+    const { name, value } = e.target;
+    setInfo({ ...info, [name]: value });
+  };
+  const navigate = useNavigate();
+  const passed = info.userId.length > 3 && info.userPassword.length > 3;
+
+  const toLogin = e => {
+    e.preventDefault();
+    fetch(`${API.LOGIN}/admin/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: info.userId,
+        password: info.userPassword,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        if (result.adminToken) {
+          localStorage.setItem('token', result.adminToken);
+          alert('로그인 성공');
+          navigate('/');
+        } else {
+          alert('아이디 혹은 비밀번호를 확인해 주세요');
+        }
+      });
+  };
+
+  return (
+    <S.Layout>
+      <S.BackgroundColor>
+        <S.LayoutCenter>
+          <S.Logo>KoDa</S.Logo>
+          <S.LoginBox>
+            <S.LoginText>로그인</S.LoginText>
+            <S.LoginForm>
+              <S.LoginId
+                type="text"
+                placeholder=" 관리자 아이디를 입력해주세요."
+                name="userId"
+                value={info.userId}
+                onChange={onChangeinfo}
+              />
+              <S.LoginPassword
+                type="password"
+                placeholder=" 비밀 번호를 입력해주세요"
+                name="userPassword"
+                value={info.userPassword}
+                onChange={onChangeinfo}
+              />
+              <S.LoginButton disabled={!passed} onClick={toLogin}>
+                완료
+              </S.LoginButton>
+            </S.LoginForm>
+          </S.LoginBox>
+        </S.LayoutCenter>
+      </S.BackgroundColor>
+    </S.Layout>
+  );
+};
+
+export default Login;
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Main = () => {
-  return <div>Main</div>;
+  return <div>Maiadfdn</div>;
 };
 
 export default Main;
