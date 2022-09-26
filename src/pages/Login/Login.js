21d8faee987a189464f883914fcd4895431e817f
Joeunji0119 <95282989+Joeunji0119@users.noreply.github.com>
2022-09-27 01:53:12 +0900

    for push

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './LoginStyle';
-import { API } from '../../../config';
+import { API } from '../../config';
 
 const Login = () => {
   const [info, setInfo] = useState({
