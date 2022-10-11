97894732fa05a24fa1834c078560766e4edb9b4a
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2022-10-11 09:26:14 +0900

    fix: almost have done

--- src/pages/user/UserContainer.js
+++ src/pages/user/UserContainer.js
@@ -9,11 +9,14 @@ const UserContainer = () => {
   const [survey, setSurvey] = useState({}); // form 데이터 이외의 State
   const location = useLocation();
   const url = location.pathname;
-
   const id = url.substring(12);
 
   useEffect(() => {
-    fetch(`${API.SURVEYPAGE}/${id}`)
+    fetch(`${API.SURVEYPAGE}/${id}`, {
+      headers: {
+        'Content-type': 'application/json',
+      },
+    })
       .then(res => res.json())
       .then(result => setForm(result));
   }, [id]);
